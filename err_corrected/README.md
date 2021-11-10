aff3a3f1063818fc1652020fb380168ee5068a47
Taehwa <81178597+kthkim050707@users.noreply.github.com>
2021-11-10 11:52:43 +0900

    Add files via upload

---
+++ err_corrected/README.md
@@ -0,0 +1,1 @@
+# jjabroschool_mh
---
+++ err_corrected/index.js
@@ -0,0 +1,144 @@
+
+	$( document ).ready(function() {
+		$("#time").hide();
+		$("#id_school").hide();
+		$("#id_zocbo").hide();
+		$("#id_riro").hide();
+		$("#id_edu").hide();
+		$("#id_gs").hide();
+		$("#background").show();
+	});
+
+	function time_sche(){
+		$("#id_school").hide();
+		$("#time").show();
+		$("#id_zocbo").hide();
+		$("#id_riro").hide();
+		$("#id_edu").hide();
+		$("#id_gs").hide();
+		$("#background").hide();
+	}
+
+	function riro(){
+		$("#id_school").hide();
+		$("#time").hide();
+		$("#id_zocbo").hide();
+		$("#id_riro").show();
+		$("#id_edu").hide();
+		$("#id_gs").hide();
+		$("#background").hide();
+	}
+
+   function school(){
+		$("#id_school").show();
+		$("#time").hide();
+		$("#id_zocbo").hide();
+		$("#id_riro").hide();
+		$("#id_edu").hide();
+		$("#id_gs").hide();
+		$("#background").hide();
+    }
+
+	function edu(){
+		$("#id_school").hide();
+		$("#time").hide();
+		$("#id_zocbo").hide();
+		$("#id_riro").hide();
+		$("#id_edu").show();
+		$("#id_gs").hide();
+		$("#background").hide();
+	}
+	function gs(){
+		$("#id_school").hide();
+		$("#time").hide();
+		$("#id_zocbo").hide();
+		$("#id_riro").hide();
+		$("#id_edu").hide();
+		$("#id_gs").show();
+		$("#background").hide();
+	}
+
+	function zocbo(){
+		$("#id_school").hide();
+		$("#time").hide();
+		$("#id_zocbo").show();
+		$("#id_riro").hide();
+		$("#id_edu").hide();
+		$("#id_gs").hide();
+		$("#background").hide();
+    }
+    function mainpage(){
+        $("#id_school").hide();
+		$("#time").hide();
+		$("#id_zocbo").hide();
+		$("#id_riro").hide();
+		$("#id_edu").hide();
+		$("#id_gs").hide();
+		$("#background").show();
+    }
+
+	function go_music(){
+
+		window.open('https://sel1.ebsoc.co.kr/class/sdmusic123');
+		
+	}
+
+	function go_history(){
+
+		window.open('https://sel1.ebsoc.co.kr/class/historyofkorean11');
+	}
+
+	function go_englishA(){
+
+		window.open('https://sel1.ebsoc.co.kr/class/sunduckenglish');
+	}
+
+	function go_experiment(){
+
+		window.open('https://sel1.ebsoc.co.kr/class/sdsciencelab11');
+	}
+
+	function go_PE(){
+
+		window.open('https://sel1.ebsoc.co.kr/class/sd1pe');
+	}
+
+	function go_society(){
+		window.open('https://sel1.ebsoc.co.kr/class/tongssa11');
+	}
+
+	function go_mathA(){
+		window.open('https://sel1.ebsoc.co.kr/class/math1a11');
+		
+	}
+
+	function go_englishB(){
+		window.open('https://sel1.ebsoc.co.kr/class/sdengb');
+	}
+
+	function go_korean(){
+		window.open('https://sel1.ebsoc.co.kr/class/sdkoreanlyb');
+	}
+
+	function go_mathB(){
+		window.open('https://sel1.ebsoc.co.kr/class/havuning1math11');
+	}
+
+	function go_cchang(){
+		window.open('https://sel1.ebsoc.co.kr/class/sdhanmoon111');
+	}
+	
+	function go_science(){
+		window.open('https://sel1.ebsoc.co.kr/class/sd111sc');
+	}
+
+	function go_dream(){
+		window.open('https://sel1.ebsoc.co.kr/class/smjinro111');
+	}
+
+	function go_class(){
+		window.open('https://sel1.ebsoc.co.kr/class/sdclass111');
+	}
+    function go_mathA10(){
+		window.open('https://sel1.ebsoc.co.kr/class/wjdgksdud10');
+	}
